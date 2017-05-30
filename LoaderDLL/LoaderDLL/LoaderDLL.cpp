4438238413ffb18030f730ed98c7f62547812999
Assan <jjbryant@fullsail.edu>
2017-05-30 12:19:24 -0400

    Test DLL Class/namespace created

--- LoaderDLL/LoaderDLL/LoaderDLL.cpp
+++ LoaderDLL/LoaderDLL/LoaderDLL.cpp
@@ -2,5 +2,22 @@
 //
 
 #include "stdafx.h"
+#include "TestHeader.h"
 
+namespace MathLibrary
+{
+	double Functions::Add(double a, double b)
+	{
+		return a + b;
+	}
 
+	double Functions::Multiply(double a, double b)
+	{
+		return a * b;
+	}
+
+	double Functions::AddMultiply(double a, double b)
+	{
+		return a + (a * b);
+	}
+}
